51c4347e9233a3131706c1b0beb7d7819e7c71b8
mogu283 <mogukona2016@gmail.com>
2020-06-24 22:19:33 +0900

    Initial Commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+ import router from './router'
+ import store from './store'
+ import firebase from 'firebase'
 
 Vue.config.productionTip = false
-
+const config = {
+   apiKey: 'AIzaSyBwdYTgkcGmXxPq9I2jiImz74vcA_2ZbN8',
+   projectId: 'test-b76fc',
+   authDomain: 'test-b76fc.firebaseapp.com',
+   databaseURL: 'https://test-b76fc.firebaseio.com',
+   storageBucket: 'test-b76fc.appspot.com',
+   };
+  
+   firebase.initializeApp(config);
+  
+   export default firebase;
 new Vue({
   router,
   store,
